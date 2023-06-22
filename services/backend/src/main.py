8102c2e3113aeddb3a883c69bce2627e5d693610
Tanner Woodrum <tanner.woodrum1@gmail.com>
2023-06-22 00:17:29 -0600

    first commit

---
+++ services/backend/src/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from src.database.register import register_tortoise  # NEW
+from src.database.config import TORTOISE_ORM         # NEW
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials = True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+register_tortoise(app, config=TORTOISE_ORM, generate_schemas=False)
+
+@app.get("/")
+def home():
+    return "Hello, World!"
