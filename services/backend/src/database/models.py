f7f890ff371a7159fc865bd77545b233f079696d
Tanner Woodrum <tanner.woodrum1@gmail.com>
2023-06-26 06:31:56 -0600

    fixing constructor bugs

--- services/backend/src/database/models.py
+++ services/backend/src/database/models.py
@@ -8,12 +8,6 @@ class Users(models.Model):
     created_at = fields.DatetimeField(auto_now_add=True)
     modified_at = fields.DatetimeField(auto_now=True)
 
-    def __init__(self, username: str, password: str, created_at: datetime.datetime, modified_at: datetime.datetime):
-        self.username = username
-        self.password = password
-        self.created_at = created_at
-        self.modified_at = modified_at
-
 
 class Tasks(models.Model):
 
@@ -28,10 +22,3 @@ class Tasks(models.Model):
 
     def __str__(self):
         return f"{self.title}, {self.user_id} on {self.created_at}"
-    
-    def __init__(self, title:str, comments:str, priority_level:int, due_date:datetime.datetime, user:Users):
-        self.title = title
-        self.comments = comments
-        self.priority_level = priority_level
-        self.due_date = due_date
-        self.user = user
