9b2c66502e772364187a9500aa31bfbdd4adc29b
Tanner Woodrum <tanner.woodrum1@gmail.com>
2023-06-27 07:19:03 -0600

    trying to get tests to work

--- services/test/tests/test_main.py
+++ services/test/tests/test_main.py
@@ -1,6 +1,9 @@
 from starlette.testclient import TestClient
+import sys
+from backend.src.main import app
 
-from services.backend.src.main import app
+sys.path.insert(0, '/Users/tannerwoodrum/Documents/Life/CodingProjects/TodoApp/services/backend')
+
 
 client = TestClient(app)
 
