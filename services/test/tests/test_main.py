5a3699acbc14b94dd3a6895835f12f92f52225a3
Tanner Woodrum <tanner.woodrum1@gmail.com>
2023-06-28 06:55:40 -0600

    Finally got the tests to run

--- services/test/tests/test_main.py
+++ services/test/tests/test_main.py
@@ -1,13 +1,12 @@
 from starlette.testclient import TestClient
 import sys
+
+
 from backend.src.main import app
-
-sys.path.insert(0, '/Users/tannerwoodrum/Documents/Life/CodingProjects/TodoApp/services/backend')
-
 
 client = TestClient(app)
 
 def test_home():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {}
+    assert response.json() == 'Hello, World!'
