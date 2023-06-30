bdc8e93c0465175862287816cf81e75f1d5df61d
Tanner Woodrum <tanner.woodrum1@gmail.com>
2023-06-30 07:20:01 -0600

    Getting the frontend set up in docker!

--- services/frontend/src/main.js
+++ services/frontend/src/main.js
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import "bootstrap/dist/css/bootstrap.css";
+import axios from "axios";
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = "http://localhost:5050/"; // the fast api backend
+
+app.use(router);
+app.mount("#app");
