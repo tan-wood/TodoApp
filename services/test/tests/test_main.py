a8dd726691c08ec7656ef74adf330d21ceb26ff1
Tanner Woodrum <tanner.woodrum1@gmail.com>
2023-07-06 21:56:12 -0600

    Got it working

--- services/test/tests/test_main.py
+++ services/test/tests/test_main.py
@@ -10,3 +10,5 @@ def test_home():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == 'Hello, World!'
+
+#TODO Make sure to put these tests into the backend project
